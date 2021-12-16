ef6f1cd59b8aeb220e13f5c7c871a53659cb80f5
CEILINGSPYSERVERS <CEILINGSPYSERVERS@Gmail.com>
2021-12-16 04:04:47 -0600

    Update remote.js

--- remote.js
+++ remote.js
@@ -1,7 +1,7 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
     var target = ns.args[0]
-    let targets = ["nectara-net", "harakiri-sushi", "hong-fang-tea", "joesguns", "sigma-cosmetics", "foodnstuff", "n00dles"]
+    let targets = ["phantasy", "neo-net", "zer0", "max-hardware", "iron-gym", "nectara-net", "harakiri-sushi", "hong-fang-tea", "joesguns", "sigma-cosmetics", "foodnstuff", "n00dles"]
     var arraylen = targets.length
     if (!ns.fileExists("steal.js")) {
         ns.wget("https://raw.githubusercontent.com/CEILINGSPYSERVERS/Bitburner/main/steal.js", "steal.js")
