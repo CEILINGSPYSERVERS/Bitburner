6b108494ae7bf12335f6432bb65b4a8432a75479
CEILINGSPYSERVERS <CEILINGSPYSERVERS@Gmail.com>
2021-12-16 02:29:03 -0600

    1

--- deploy.js
+++ deploy.js
@@ -1,11 +1,11 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
-    var target = ns.args[0];
+    var target = ns.args[0]
     ns.connect(target)
     if (ns.fileExists("test.js", target)) {
-        ns.run(test.js, 1, (target));
+        ns.run(test.js, 1, (target))
     } else {
-        ns.wget("https://raw.githubusercontent.com/CEILINGSPYSERVERS/Bitburner/main/test.js", test.js)
+        ns.wget("https://raw.githubusercontent.com/CEILINGSPYSERVERS/Bitburner/main/remote.js", remote.js)
     }
-    ns.run(test.js, 1, target);
+    ns.run(remote.js, 1, target)
 }
