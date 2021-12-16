500bda8561f4a3fe771791b4990ed7189c2ef3b6
CEILINGSPYSERVERS <CEILINGSPYSERVERS@Gmail.com>
2021-12-16 00:43:02 -0600

    1

--- template.js
+++ template.js
@@ -1,8 +1,4 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
-    if (ns.fileExists("test.js", target)) {
-        ns.run(test.js, 1, (target));
-    } else {
-        ns.wget("URL", test.js)
-    }
+    
 }
