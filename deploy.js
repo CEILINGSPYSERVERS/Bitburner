4d9e32a3ea99ad5c1a8b1156ee35d92e40fd4e4d
CEILINGSPYSERVERS <CEILINGSPYSERVERS@Gmail.com>
2021-12-16 00:54:36 -0600

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,5 +1,6 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
+    ns.connect(target)
     if (ns.fileExists("test.js", target)) {
         ns.run(test.js, 1, (target));
     } else {
