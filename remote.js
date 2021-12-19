05ee5a1b3456e08648c0ea824563b17ccd0425ef
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-12-19 00:43:21 -0600

    Update remote.js

--- remote.js
+++ remote.js
@@ -3,10 +3,7 @@
 export async function main(ns) {
     var target = ns.args[0]
     await ns.wget("https://raw.githubusercontent.com/CEILINGSPYSERVERS/Bitburner/main/steal.js", "steal.js")
-    let ramreq = ns.getScriptRam("steal.js", target)
-    let maxram = ns.getServerMaxRam(target)
-    let ramused = ns.getServerUsedRam(target)
-    let freeram = maxram - ramused
-    let threads = freeram / ramreq
+    let threads = ns.getServerUsedRam(target) / ns.getScriptRam("steal.js", target)
+    ns.tprint("Running steal.js on "+threads+" threads.")
     ns.run("steal.js", threads, target)
 }
