e1375c0ecbb480da09e35f73c6f0ebad199df383
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-12-20 00:23:51 -0600

    1

--- hack.js
+++ hack.js
@@ -2,20 +2,25 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
     var target = ns.args[0]
-    if (ns.fileExists("BruteSSH.exe", "home")) {
+    if (ns.fileExists("brutessh.exe", "home")) {
         ns.brutessh(target)
+        ns.tprint("BruteSSH "+target)
     }
     if (ns.fileExists("ftpcrack.exe", "home")) {
         ns.ftpcrack(target)
+        ns.tprint("FTPCrack "+target)
     }
     if (ns.fileExists("relaysmtp.exe", "home")) {
         ns.relaysmtp(target)
+        ns.tprint("RelaySMTP "+target)
     }
     if (ns.fileExists("httpworm.exe", "home")) {
         ns.httpworm(target)
+        ns.tprint("HTTPWorm "+target)
     }
     if (ns.fileExists("sqlinject.exe", "home")) {
         ns.sqlinject(target)
+        ns.tprint("SQLInject "+target)
     }
     ns.nuke(target)
 }
