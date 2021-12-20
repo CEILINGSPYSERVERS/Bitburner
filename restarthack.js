1286e5ac24f6b512c4a621782ee881429d0b9f4e
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-12-19 23:55:30 -0600

    Update restarthack.js

--- restarthack.js
+++ restarthack.js
@@ -3,12 +3,12 @@
 export async function main(ns) {
     let port = ns.args[0]
     let ports = []
-    ports0 = ["n00dles","foodnstuff","sigma-cosmetics","joesguns","nectar-net","hong-fang-tea","harakiri-sushi"]
-    ports1 = ["neo-net","zer0","max-hardware","iron-gym","CSEC","neo-net"]
-    ports2 = ["phantasy","silver-helix","omega-net","crush-fitness","johnson-ortho","the-hub","avmnite-02h"]
-    ports3 = ["comptek","netlink","rothman-uni","catalyst","summit-uni","rho-construction","millenium-fitness","I.I.I.I"]
-    ports4 = ["aevum-police","alpha-ent","syscore","lexo-corp","snap-fitness","global-pharm","applied-energetics","unitalife","univ-energy","nova-med","zb-def","run4theh111z","."]
-    ports5 = ["zb-institute","vitalife","titan-labs","solaris","microdyne","helios","deltaone","icarus","zeus-med","omnia","defcomm","galactic-cyber","infocomm","taiyang-digital","stormtech","aerocorp","clarkinc","omnitek","nwo","4sigma","blade","b-and-a","ecorp","fulcrumtech","megacorp","kuai-gong","fulcrumassets","powerhouse-fitness"]
+    ports[0] = ["n00dles","foodnstuff","sigma-cosmetics","joesguns","nectar-net","hong-fang-tea","harakiri-sushi"]
+    ports[1] = ["neo-net","zer0","max-hardware","iron-gym","CSEC","neo-net"]
+    ports[2] = ["phantasy","silver-helix","omega-net","crush-fitness","johnson-ortho","the-hub","avmnite-02h"]
+    ports[3] = ["comptek","netlink","rothman-uni","catalyst","summit-uni","rho-construction","millenium-fitness","I.I.I.I"]
+    ports[4] = ["aevum-police","alpha-ent","syscore","lexo-corp","snap-fitness","global-pharm","applied-energetics","unitalife","univ-energy","nova-med","zb-def","run4theh111z","."]
+    ports[5] = ["zb-institute","vitalife","titan-labs","solaris","microdyne","helios","deltaone","icarus","zeus-med","omnia","defcomm","galactic-cyber","infocomm","taiyang-digital","stormtech","aerocorp","clarkinc","omnitek","nwo","4sigma","blade","b-and-a","ecorp","fulcrumtech","megacorp","kuai-gong","fulcrumassets","powerhouse-fitness"]
 
     for (let i = 0; i < ports[port].length; i++) {
         ns.run("hack.js", 1, ports[port][i])
