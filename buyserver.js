db58282fae1eb6b762054b405d2af1811802cfbc
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-12-27 14:01:30 -0600

    1

---
+++ buyserver.js
@@ -0,0 +1,12 @@
+//wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/Bitburner/main/buyserver.js buyserver.js
+/** @param {import(".").NS } ns */
+export async function main(ns) {
+	let ram = ns.args[0]
+	let cost = ns.getPurchasedServerCost(ram)
+	let money = ns.getServerMoneyAvailable("home")
+	if (money <= cost) {
+        ns.tprint("Not enough money to buy "+ram+" GB Server")
+    } else {
+        ns.purchaseServer("S", ram)
+    }
+}
