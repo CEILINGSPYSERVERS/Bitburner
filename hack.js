b4edb38dbe1d8901897205edb267d14c6a06703c
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2022-01-04 23:02:25 -0600

    Update hack.js

--- hack.js
+++ hack.js
@@ -2,25 +2,15 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
     var target = ns.args[0]
-    if (ns.fileExists("brutessh.exe", "home")) {
-        ns.brutessh(target)
-        ns.tprint("BruteSSH "+target)
-    }
-    if (ns.fileExists("ftpcrack.exe", "home")) {
-        ns.ftpcrack(target)
-        ns.tprint("FTPCrack "+target)
-    }
-    if (ns.fileExists("relaysmtp.exe", "home")) {
-        ns.relaysmtp(target)
-        ns.tprint("RelaySMTP "+target)
-    }
-    if (ns.fileExists("httpworm.exe", "home")) {
-        ns.httpworm(target)
-        ns.tprint("HTTPWorm "+target)
-    }
-    if (ns.fileExists("sqlinject.exe", "home")) {
-        ns.sqlinject(target)
-        ns.tprint("SQLInject "+target)
+    let files = ["BruteSSH.exe", "FTPCrack.exe", "RelaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
+    let programs = ["brutessh", "ftpcrack", "relaysmtp", "httpworm", "sqlinject"]
+    var arraylen = files.length
+    for (var i = 0; i < arraylen; i++) {
+        if (ns.fileExists(files[i], "home")) {
+            await ns.programs[i](target)
+            ns.tprint(programs[i]+" "+target)
+        }
+        
     }
     ns.nuke(target)
     ns.tprint("Nuked "+target)
