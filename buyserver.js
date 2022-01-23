8c9c65f86edfbdf4bdd8c568b4de04925f901de3
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2022-01-23 00:52:03 -0600

    1

--- buyserver.js
+++ buyserver.js
@@ -3,6 +3,14 @@
 export async function main(ns) {
 	let money = ns.getServerMoneyAvailable("home")
 	let ram = money / 55000
+	let power = 1
+    let exponent = 1
+    while (power < ram) {
+        power *= 2
+        exponent += 1
+    }
+    exponent -= 2
+    ram = 2 ** exponent
 	let cost = ns.getPurchasedServerCost(ram)
 	ns.tprint("Bought "+ram+" GB server for "+cost)
 	ns.purchaseServer("S", ram)
--- servertest.js
+++ servertest.js
@@ -2,7 +2,15 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
     let money = ns.getServerMoneyAvailable("home")
-	let ram = money / 55000
+    let ram = money / 55000
+    let power = 1
+    let exponent = 1
+    while (power < ram) {
+        power *= 2
+        exponent += 1
+    }
+    exponent -= 2
+    ram = 2 ** exponent
     let cost = ns.getPurchasedServerCost(ram)
-    ns.tprint("Can buy "+ram+" GB server for "+cost)
+    ns.tprint("Can buy " + ram + " GB server for " + cost)
 }
